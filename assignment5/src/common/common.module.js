de82945efb25021def4e44f4dbc62ec810ef030f
Shashank Kumar <shashank136.sk@gmail.com>
2018-11-18 17:45:41 +0530

    Update common.module.js

--- assignment5/src/common/common.module.js
+++ assignment5/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://aleksandar-gjorgievski.herokuapp.com')
+.constant('ApiPath', 'https://shashank136-course5.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
